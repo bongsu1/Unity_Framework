bb829503b59887316230c2d202f93e137817afa2
bongsu <rlaqhdtn22@gmail.com>
2025-01-06 15:34:16 +0900

    SoundManager 스크립트 작성
    오디오 파일 재생 및 조절 기능 구현

--- Assets/Scripts/Manager/Manager.cs
+++ Assets/Scripts/Manager/Manager.cs
@@ -5,6 +5,7 @@ public class Manager
     public static GameManager Game { get { return GameManager.Instance; } }
     public static ResourceManager Resource { get { return ResourceManager.Instance; } }
     public static SceneManager Scene { get { return SceneManager.Instance; } }
+    public static SoundManager Sound { get { return SoundManager.Instance; } }
     public static UIManager UI { get { return UIManager.Instance; } }
 
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSplashScreen)]
@@ -13,11 +14,13 @@ public class Manager
         GameManager.ReleaseInstance();
         ResourceManager.ReleaseInstance();
         SceneManager.ReleaseInstance();
+        SoundManager.ReleaseInstance();
         UIManager.ReleaseInstance();
 
         GameManager.CreateInstance();
         ResourceManager.CreateInstance();
         SceneManager.CreateInstance();
+        SoundManager.CreateInstance();
         UIManager.CreateInstance();
     }
 }
---
+++ Assets/Scripts/Manager/SoundManager.cs
@@ -0,0 +1,85 @@
+using System;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundManager : Singleton<SoundManager>
+{
+    AudioSource[] _audioSources = null;
+
+    /// <summary>
+    /// Sound Volume
+    /// </summary>
+    public float this[SoundType type]
+    {
+        get { return _audioSources[(int)type].volume; }
+        set { _audioSources[(int)type].volume = value; }
+    }
+
+    protected override void Init()
+    {
+        base.Init();
+
+        string[] soundTypeNames = Enum.GetNames(typeof(SoundType));
+        _audioSources = new AudioSource[soundTypeNames.Length];
+        for (int i = 0; i < soundTypeNames.Length; i++)
+        {
+            GameObject obj = new GameObject { name = soundTypeNames[i] };
+            _audioSources[i] = Util.GetOrAddComponent<AudioSource>(obj);
+            obj.transform.SetParent(transform);
+        }
+
+        _audioSources[(int)SoundType.BGM].loop = true;
+    }
+
+    public void Play(SoundType type, string path)
+    {
+        if (string.IsNullOrEmpty(path))
+            return;
+
+        AudioClip clip = Manager.Resource.Load<AudioClip>(path);
+        if (clip == null)
+            return;
+
+        AudioSource audioSource = _audioSources[(int)type];
+
+        switch (type)
+        {
+            case SoundType.BGM:
+                if (audioSource.isPlaying)
+                    audioSource.Stop();
+
+                audioSource.clip = clip;
+                audioSource.Play();
+                break;
+            case SoundType.Effect:
+                audioSource.PlayOneShot(clip);
+                break;
+            default:
+                // SoundType 추가 시 수정
+                // nothing
+                break;
+        }
+    }
+
+    public void Stop(SoundType type)
+    {
+        AudioSource audioSource = _audioSources[(int)type];
+        audioSource.Stop();
+    }
+
+    public void SetPitch(SoundType type, float pitch = 1f)
+    {
+        AudioSource audioSource = _audioSources[(int)type];
+        audioSource.pitch = pitch;
+    }
+
+    public void Clear()
+    {
+        for (int i = 0; i < _audioSources.Length; i++)
+        {
+            _audioSources[i].Stop();
+        }
+    }
+}
+
+public enum SoundType { BGM, Effect, }
